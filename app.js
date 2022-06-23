28be0d6b97c44ede942a12679e7d240c2ee20ef7
ObodoakorDavid <obodoakordavid@outlook.com>
2022-06-22 23:40:51 -1100

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+/** @format */
+
+let MyName = "Obodoakor David";
+let height = 6.1;
+let country = "Nigeria";
+
+console.log("My name is " + MyName);
+console.log("I am " + height + " feet Tall");
+console.log("I am from " + country);
